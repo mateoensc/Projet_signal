25ce25c664a8d4c384bb3790196495350dd08dc0
Marie Appaailly <mapparailly@ensc.fr>
2022-10-21 11:41:37 +0200

    Détecteur de Harris multi-échelle et affichage

--- Binome1.m
+++ Binome1.m
@@ -45,28 +45,42 @@ sigmaG = 2.1;
 delta = ceil(3*sigmaG); % arrondi au supérieur pour que la fenêtre comprenne l'intervalle [-3sig;3sig]
 [X,Y] = meshgrid( -delta:delta , -delta:delta );
 
-G_X = - X./ ((2*pi*sigmaG^4) * exp(-(X.^2 + Y.^2)/(2*sigmaG^2)));
-G_Y = - Y / (2*pi*sigmaG^4) * exp(-(X.^2 + Y.^2)/(2*sigmaG^2));
+G_X = - X/ (2*pi*sigmaG^4) .*(exp(-(X.^2 + Y.^2)/(2*sigmaG^2)));
+G_Y = - Y/ (2*pi*sigmaG^4) .*(exp(-(X.^2 + Y.^2)/(2*sigmaG^2)));
+
 
 % convolution
-gradX = conv2(imgGray,G_X);
-gradY= conv2(imgGray,G_Y);
+gradX = conv2(imgGray,G_X,'same'); % A VOIR DANS COURS
+gradY = conv2(imgGray,G_Y,'same');
 
-figure, image(gradX), title("Composantes horizontales");
-figure, image(gradY), title("Composantes verticales");
+figure, imshow(gradX), title("Composantes horizontales");
+figure, imshow(gradY), title("Composantes verticales");
 
 % covariance des gradients
-% (approche de Cany : gradient I estimé par convo des dérives partielles de G sur I)
+% (approche de Canny : gradient I estimé par convo des dérives partielles de G sur I)
 IxIx = gradX.* gradX;
 IyIy = gradY.* gradY;
 IxIy = gradX.*gradY;  % = IyIx
 
-sigmaC = 3;
-G = (1 / (2*pi*sigmaC^2)) * exp(-(X.^2+Y.^2)/(2*sigmaC^2));
+sigmaC1 = 3;
+GC1 = (1 / (2*pi*sigmaC1^2)) * exp(-(X.^2+Y.^2)/(2*sigmaC1^2));
 
-Cxx = conv2(IxIx,G);
-Cyy = conv2(IyIy,G);
-Cxy = conv2(IxIy,G);  % = Cyx
+Cxx1 = conv2(IxIx,GC1,'same');
+Cyy1 = conv2(IyIy,GC1,'same');
+Cxy1 = conv2(IxIy,GC1,'same');  % = Cyx
+
+sigmaC2 = 5;
+GC2 = (1 / (2*pi*sigmaC2^2)) * exp(-(X.^2+Y.^2)/(2*sigmaC2^2));
+Cxx2 = conv2(IxIx,GC2,'same');
+Cyy2 = conv2(IyIy,GC2,'same');
+Cxy2 = conv2(IxIy,GC2,'same');  % = Cyx
 
 % Calcul du détecteur en tout point
-DetHarris1 = Cxx.*Cyy - Cxy.^2 - k.* (Cxx + Cyy).^2;
+k = 0.05;
+DetTrace1 = Cxx1.*Cyy1 - Cxy1.*Cxy1 - k.* ((Cxx1 + Cyy1).^2);
+DetTrace2 = Cxx2.*Cyy2 - Cxy2.*Cxy2 - k.* ((Cxx2 + Cyy2).^2);
+
+% combinaison multi-échelle
+Dmulti = min(DetTrace1 .* abs(DetTrace2), abs(DetTrace1) .* DetTrace2);
+
+figure, imshow(Dmulti), title("Détecteur de Harris");
